508c1612ca790f8a95bac372a2c9d5b40ed3eac1
Horea Christian <h.chr@mail.ru>
2014-11-15 21:52:22 +0100

    started rewriting beta binomial mor multi-precision

--- functions.py
+++ functions.py
@@ -1,5 +1,17 @@
-def beta_binomial(k,n,a,b):
-	import scipy.special.beta as B
-	import
-	p = choose(n,k) * B(k+a, n-k+b) / B(a,b)
+def mp_beta_binomial(k,n,a,b):
+	from scipy.special import beta
+	from scipy.misc import comb
+	import mpmath
+	p = comb(n,k) * mpmath.beta(k+a, n-k+b) / mpmath.beta(a,b)
 	return p
+
+def mp_comb(n,k):
+	from numpy import asarray
+	k,N = asarray(k), asarray(N)
+	cond = (k <= N) & (N >= 0) & (k >= 0)
+	vals = binom(N, k)
+	if isinstance(vals, np.ndarray):
+	    vals[~cond] = 0
+	elif not cond:
+	    vals = np.float64(0)
+	return vals
