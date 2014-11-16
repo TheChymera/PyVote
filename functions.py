f5106fd3a95abfc25dde5f42d5d5b32f2320a884
Horea Christian <h.chr@mail.ru>
2014-11-16 11:49:05 +0100

    added optional multiprecision floating-point functionality

--- functions.py
+++ functions.py
@@ -1,17 +1,14 @@
-def mp_beta_binomial(k,n,a,b):
-	from scipy.special import beta
-	from scipy.misc import comb
-	import mpmath
-	p = comb(n,k) * mpmath.beta(k+a, n-k+b) / mpmath.beta(a,b)
+def beta_binomial(k,n,a,b,mp=False):
+	if mp:
+		import mpmath
+		p = mp_comb(n,k) * mpmath.beta(k+a, n-k+b) / mpmath.beta(a,b)
+	else:
+		from scipy.special import beta
+		from scipy.misc import comb
+		p = comb(n,k) * beta(k+a, n-k+b) / beta(a,b)
 	return p
 
-def mp_comb(n,k):
-	from numpy import asarray
-	k,N = asarray(k), asarray(N)
-	cond = (k <= N) & (N >= 0) & (k >= 0)
-	vals = binom(N, k)
-	if isinstance(vals, np.ndarray):
-	    vals[~cond] = 0
-	elif not cond:
-	    vals = np.float64(0)
+def mp_comb(N,k):
+	import mpmath
+	vals = mpmath.factorial(N)/(mpmath.factorial(k)*mpmath.factorial(N-k))
 	return vals
