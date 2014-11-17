661fc41072231304ee0b4250a2dfccb26c2e05a5
Horea Christian <h.chr@mail.ru>
2014-11-17 03:06:46 +0100

    added cli tool for one vote

---
+++ 1vote_cli.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+__author__ = 'Horea Christian'
+import argparse
+import votes
+
+parser = argparse.ArgumentParser()
+parser.add_argument("population", help="The projected population turnout.", type=int)
+parser.add_argument("-t", "--threshold", help="Threshold needed for reference candidate to win", type=float)
+parser.add_argument("-b", "--beta", help="Beta distribution parameters: alpha and beta.", nargs=2, type=float)
+parser.add_argument("-f", "--forecasts", help="List with polling forecasts.", nargs='+', type=float)
+parser.add_argument("-n", "--normal", help="Normal distribution fitted to forecast data: mean and standard deviation", nargs=2, type=float)
+parser.add_argument("-p", "--probability", help="Fixed forecast (not recommended)", type=float)
+parser.add_argument("-d", "--diagnostics", help="Display beta function plot for diagnostic purposes", action="store_true")
+args = parser.parse_args()
+
+print args.population
+print args.threshold
+print args.beta
+print args.forecasts
+print args.normal
+print args.probability
+print args.diagnostics
+
+# votes.one_vote(args.population, args.threshold, ab=args.iptc_profile, do_minis=args.do_minis, do_fullsize=args.do_fullsize, template_name=args.template, mini_width=args.mini_width)
