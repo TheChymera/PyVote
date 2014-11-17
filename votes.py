666bfe92dee801635276354170674f99e6564adf
Horea Christian <h.chr@mail.ru>
2014-11-17 03:07:17 +0100

    typos

--- votes.py
+++ votes.py
@@ -25,13 +25,15 @@ def one_vote(N, threshold=0.5, ab=False, forecasts=False, ms=False, p=False, dia
 			raise ValueError("Please specify only one of the 'ab', 'forecasts', 'ms', and 'p' arguments.")
 		from scipy.stats import binom
 		pr = binom.pmf(np.ceil(N*threshold),N,p)
-		if (N*threshold).is_integer():
-			pr += binom.pmf(N*threshold+1,N,p)
+		if (N*threshold).is_integer() or threshold != 0.5:
+			# adjust for tie probability
+			pr += binom.pmf(N*(1-threshold)+1,N,p)
 	if not pr:
 		from functions import beta_binomial
 		pr = beta_binomial(np.ceil(N*threshold),N,a,b,mp=True)
-		if (N*threshold).is_integer():
-			pr += beta_binomial(N*threshold+1,N,a,b,mp=True)
+		if (N*threshold).is_integer() or threshold != 0.5:
+			# adjust for tie probability
+			pr += beta_binomial(N*(1-threshold)+1,N,a,b,mp=True)
 
 	if diagnostic:
 		import matplotlib.pyplot as plt
