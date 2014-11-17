12e297f1b0e242f5198cb63d923dcad4d7162b31
Horea Christian <h.chr@mail.ru>
2014-11-17 15:55:43 +0100

    corrected tie probability conditions and calculations

--- votes.py
+++ votes.py
@@ -3,7 +3,7 @@ __author__ = 'Horea Christian'
 
 def one_vote(N, threshold=0.5, ab=False, forecasts=False, ms=False, p=False, diagnostic=False):
 	import numpy as np
-	a = False
+	a = b = False
 	pr = False
 	if not (ab or forecasts or ms or p):
 		raise ValueError("You need to specify either 'ab', 'forecasts', 'ms', or 'p'.")
@@ -24,16 +24,27 @@ def one_vote(N, threshold=0.5, ab=False, forecasts=False, ms=False, p=False, dia
 		if a:
 			raise ValueError("Please specify only one of the 'ab', 'forecasts', 'ms', and 'p' arguments.")
 		from scipy.stats import binom
-		pr = binom.pmf(np.ceil(N*threshold),N,p)
-		if (N*threshold).is_integer() or threshold != 0.5:
-			# adjust for tie probability
-			pr += binom.pmf(N*(1-threshold)+1,N,p)
-	if not pr:
+		victory_pr = binom.pmf(np.ceil(N*threshold),N,p)
+		if (N*threshold).is_integer():
+			# tie probability in case N*threshold is whole:
+			tie_pr = binom.pmf(N*(1-threshold)+1,N,p)
+		elif not (N*threshold).is_integer() and threshold != 0.5:
+			# tie probability in case N*threshold is not whole:
+			tie_pr = binom.pmf(N*(1-threshold),N,p)
+		else:
+			tie_pr = 0
+
+	if a and b:
 		from functions import beta_binomial
-		pr = beta_binomial(np.ceil(N*threshold),N,a,b,mp=True)
-		if (N*threshold).is_integer() or threshold != 0.5:
-			# adjust for tie probability
-			pr += beta_binomial(N*(1-threshold)+1,N,a,b,mp=True)
+		victory_pr = beta_binomial(np.ceil(N*threshold),N,a,b,mp=True)
+		if (N*threshold).is_integer():
+			# tie probability in case N*threshold is whole:
+			tie_pr = beta_binomial(N*(1-threshold)+1,N,a,b,mp=True)
+		elif not (N*threshold).is_integer() and threshold != 0.5:
+			# tie probability in case N*threshold is not whole:
+			tie_pr = beta_binomial(N*(1-threshold),N,a,b,mp=True)
+		else:
+			tie_pr = 0
 
 	if diagnostic:
 		import matplotlib.pyplot as plt
@@ -49,4 +60,5 @@ def one_vote(N, threshold=0.5, ab=False, forecasts=False, ms=False, p=False, dia
 		plt.ylabel('PDF')
 		plt.show()
 
+	pr = victory_pr+tie_pr
 	return pr
