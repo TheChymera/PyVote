05accdff9c6c3d911ff227f0cc13dae8d8d4a1cd
Horea Christian <h.chr@mail.ru>
2014-11-18 06:22:13 +0100

    reformatted argument parsing

--- votes.py
+++ votes.py
@@ -3,26 +3,23 @@ __author__ = 'Horea Christian'
 
 def one_vote(N, threshold=0.5, ab=False, forecasts=False, normal=False, p=False, diagnostic=False):
 	import numpy as np
-	a = b = False
-	pr = False
-	if not (ab or forecasts or normal or p):
-		raise ValueError("You need to specify either 'ab', 'forecasts', 'ms', or 'p'.")
+
+	if [ab, forecasts, normal, p].count(True) != 1:
+		raise ValueError("Please specify one and only one of the 'ab', 'forecasts', 'normal', or 'p' options.")
+
 	if ab:
 		a, b = ab
 	elif forecasts:
 		from scipy.stats import beta
-		if a:
-			raise ValueError("Please specify only one of the 'ab', 'forecasts', 'ms', and 'p' arguments.")
 		a, b, _, _ = beta.fit(forecasts, floc=0, fscale=1)
 	elif normal:
-		if a:
-			raise ValueError("Please specify only one of the 'ab', 'forecasts', 'ms', and 'p' arguments.")
 		from functions import fit_beta_to_normal
 		m, s = normal
 		a, b = fit_beta_to_normal(m,s)
-	elif p:
-		if a:
-			raise ValueError("Please specify only one of the 'ab', 'forecasts', 'ms', and 'p' arguments.")
+	else:
+		pass
+
+	if p:
 		from scipy.stats import binom
 		victory_pr = binom.pmf(np.ceil(N*threshold),N,p)
 		if (N*threshold).is_integer():
@@ -33,8 +30,7 @@ def one_vote(N, threshold=0.5, ab=False, forecasts=False, normal=False, p=False,
 			tie_pr = binom.pmf(N*(1-threshold),N,p)
 		else:
 			tie_pr = 0
-
-	if a and b:
+	elif a and b:
 		from functions import beta_binomial
 		victory_pr = beta_binomial(np.ceil(N*threshold),N,a,b,mp=True)
 		if (N*threshold).is_integer():
@@ -45,6 +41,8 @@ def one_vote(N, threshold=0.5, ab=False, forecasts=False, normal=False, p=False,
 			tie_pr = beta_binomial(N*(1-threshold),N,a,b,mp=True)
 		else:
 			tie_pr = 0
+	else:
+		pass
 
 	if diagnostic:
 		import matplotlib.pyplot as plt
