40b7da4d154562c7fce65f31d284d2126d162ad6
Horea Christian <h.chr@mail.ru>
2014-11-19 23:33:35 +0100

    reformatted forecast flag logic

--- 1vote_cli.py
+++ 1vote_cli.py
@@ -7,7 +7,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("population", help="The projected population turnout.", type=int)
 parser.add_argument("-t", "--threshold", help="Threshold needed for reference candidate to win", default=0.5, type=float)
 parser.add_argument("-b", "--beta", help="Beta distribution parameters: alpha and beta.", nargs=2, type=float)
-parser.add_argument("-f", "--forecasts", help="List with polling forecasts.", nargs='+', type=float)
+parser.add_argument("-f", "--forecasts", help="Polling forecast (repeat flag for multiple forecasts).", type=float, action="append")
 parser.add_argument("-n", "--normal", help="Normal distribution fitted to forecast data: mean and standard deviation", nargs=2, type=float)
 parser.add_argument("-p", "--probability", help="Fixed forecast (not recommended)", type=float)
 parser.add_argument("-d", "--diagnostic", help="Display beta function plot for diagnostic purposes", action="store_true")
@@ -17,7 +17,7 @@ args = parser.parse_args()
 if sum(map(bool,[args.beta, args.forecasts, args.normal, args.probability])) != 1:
 	print "Please specify one, and only one of the '--beta'/'-b', '--forecasts'/'-f', '--normal'/'-n', and '--probability'/'-p' options."
 
-pr = votes.one_vote(args.population, args.threshold, ab=args.beta, forecasts=args.forecasts, normal=args.normal, p=args.probability, diagnostic=args.diagnostic)
+pr,_,_ = votes.one_vote(args.population, args.threshold, ab=args.beta, forecasts=args.forecasts, normal=args.normal, p=args.probability, diagnostic=args.diagnostic)
 if args.forecasts:
 	forecast_str = "forecasts of"
 	for ix, i in enumerate(args.forecasts):
