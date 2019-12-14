e1b8f27b75e74250a089f58a9f6929b3c5ec14f3
Horea Christian <chr@chymera.eu>
2019-12-14 10:04:04 +0100

    Added autogenerated Bash interface

--- pyvote/votes.py
+++ pyvote/votes.py
@@ -1,16 +1,16 @@
 import argh
+import numpy as np
 
-@argh.arg('-f','--forecasts', nargs='*', type=str)
-def one_vote(N,
+def one_vote(population,
 	threshold=0.5,
 	ab=[],
 	forecasts=[],
 	normal=[],
 	p=False,
 	diagnostic=False,
+	verbose=False,
 	):
 
-	import numpy as np
 	if sum(map(bool,[ab, forecasts, normal, p])) != 1:
 		raise ValueError("Please specify one and only one of the 'ab', 'forecasts', 'normal', or 'p' options.")
 
@@ -28,24 +28,24 @@ def one_vote(N,
 
 	if p:
 		from functions import mp_binom
-		victory_pr = mp_binom(np.ceil(N*threshold),N,p)
-		if (N*threshold).is_integer():
-			# tie probability in case N*threshold is whole:
-			tie_pr = mp_binom(N*(1-threshold)+1,N,p)
-		elif not (N*threshold).is_integer() and threshold != 0.5:
-			# tie probability in case N*threshold is not whole:
-			tie_pr = mp_binom(N*(1-threshold),N,p)
+		victory_pr = mp_binom(np.ceil(population*threshold),population,p)
+		if (population*threshold).is_integer():
+			# tie probability in case population*threshold is whole:
+			tie_pr = mp_binom(population*(1-threshold)+1,population,p)
+		elif not (population*threshold).is_integer() and threshold != 0.5:
+			# tie probability in case population*threshold is not whole:
+			tie_pr = mp_binom(population*(1-threshold),population,p)
 		else:
 			tie_pr = 0
 	elif a and b:
 		from functions import beta_binomial
-		victory_pr = beta_binomial(np.ceil(N*threshold),N,a,b,multi_precission=True)
-		if (N*threshold).is_integer():
-			# tie probability in case N*threshold is whole:
-			tie_pr = beta_binomial(N*(1-threshold)+1,N,a,b,multi_precission=True)
-		elif not (N*threshold).is_integer() and threshold != 0.5:
-			# tie probability in case N*threshold is not whole:
-			tie_pr = beta_binomial(N*(1-threshold),N,a,b,multi_precission=True)
+		victory_pr = beta_binomial(np.ceil(population*threshold),population,a,b,multi_precission=True)
+		if (population*threshold).is_integer():
+			# tie probability in case population*threshold is whole:
+			tie_pr = beta_binomial(population*(1-threshold)+1,population,a,b,multi_precission=True)
+		elif not (population*threshold).is_integer() and threshold != 0.5:
+			# tie probability in case population*threshold is not whole:
+			tie_pr = beta_binomial(population*(1-threshold),population,a,b,multi_precission=True)
 		else:
 			tie_pr = 0
 	else:
@@ -65,5 +65,5 @@ def one_vote(N,
 		plt.ylabel('PDF')
 		plt.show()
 
-	total_pr = victory_pr+tie_pr
-	return total_pr, victory_pr, tie_pr
+	change_pr = victory_pr+tie_pr
+	return change_pr, victory_pr, tie_pr
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+packages = find_packages(exclude=('samri.tests*', 'samri.*.tests*'))
+
+setup(
+	name="pyvote",
+	version="9999",
+	description = "Python script suite to model vote impact probabilities. ",
+	author = "Horea Christian",
+	author_email = "chr@chymera.eu",
+	url = "https://github.com/TheChymera/pyvote",
+	keywords = ["voting", "probabilities", "political economy", "election"],
+	classifiers = [],
+	install_requires = [],
+	provides = ["pyvote"],
+	packages = packages,
+	include_package_data=True,
+	extras_require = {
+		},
+	entry_points = {'console_scripts' : \
+			['pyvote = pyvote.cli:main']
+		},
+	)
