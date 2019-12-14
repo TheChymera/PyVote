782a46de137dffec90066255050a69842d050493
Horea Christian <chr@chymera.eu>
2019-12-14 02:12:31 +0100

    Docstring style

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 def beta_binomial(k,n,a,b,multi_precission=False):
 	"""
 	Beta binomial function, returning the probability of k successes in n trials (given a p distribution beta of parameters a and b), and supporting multiprecission output.
+
 	Parameters
 	----------
 	k : int, ndarray
@@ -11,10 +12,12 @@ def beta_binomial(k,n,a,b,multi_precission=False):
 		Parameters of the beta distribution.
 	multi_precission : bool, optional
 		Whether or not to use multiprecision floating-point output (default: False).
+
 	Returns
 	-------
 	p : int,ndarray
 		Probability of k successes in n trials.
+
 	Examples
 	--------
 	>>> n = 80000
@@ -35,20 +38,24 @@ def mp_comb(N,k):
 	"""
 	The number of combinations of N things taken k at a time, with multiprecision floating-point output.
 	This is often expressed as "N choose k".
+
 	Parameters
 	----------
 	N : int, ndarray
 		Number of things.
 	k : int, ndarray
 		Number of elements taken.
+
 	Returns
 	-------
 	val : int,
 		The total number of combinations, with floating-point multiprecision.
+
 	Notes
 	-----
 	- Array arguments accepted only for exact=False case.
 	- If k > N, N < 0, or k < 0, then a 0 is returned.
+
 	Examples
 	--------
 	>>> n = 80000
@@ -63,6 +70,7 @@ def mp_comb(N,k):
 def mp_binom(k,n,p):
 	"""
 	Binomial function, returning the probability of k successes in n trials given the trial success probability p, and supporting multiprecission output.
+
 	Parameters
 	----------
 	k : int, ndarray
@@ -71,10 +79,12 @@ def mp_binom(k,n,p):
 		Trials.
 	p : float,ndarray
 		Trial (experiment) success probability.
+
 	Returns
 	-------
 	val : float,ndarray
 		Probability of k successes in n trials.
+
 	Examples
 	--------
 	>>> k = 10
@@ -90,16 +100,19 @@ def mp_binom(k,n,p):
 def fit_beta_to_normal(m,s):
 	"""
 	Calculate the a, b parameters of a beta function using the mean and variance (formula according to http://stats.stackexchange.com/a/12239/32504).
+
 	Parameters
 	----------
 	m : float, ndarray
 		Distribution mean.
 	s : float, ndarray
 		Distribution standard deviation.
+
 	Returns
 	-------
 	a,b : float, tuple
 		A and b parameters for the equivalent beta distribution.
+
 	Examples
 	--------
 	>>> m = 0.4
